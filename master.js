4774db8228f9ab996975bd4bc8a58d2795e0ed26
stummyhurt <seantsusmilch@gmail.com>
2020-06-27 17:20:42 -0500

    smooth scroll to top

--- master.js
+++ master.js
@@ -45,6 +45,7 @@ function onScroll() {
 }
 
 function scrollToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    // document.body.scrollTop = 0;
+    // document.documentElement.scrollTop = 0;
+    window.scrollTo({top: 0, behavior: 'smooth'});
 }
